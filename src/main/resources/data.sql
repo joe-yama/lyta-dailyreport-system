b9fcd57005179af750221cf83a188cd17cc4830d
joe-yama <s1r0mqme@gmail.com>
2025-01-26 23:50:38 +0900

    Feat: #5 add logical deleted item into reports table

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -9,3 +9,5 @@ INSERT INTO daily_report_system.reports(report_date,title,content,employee_code,
      VALUES ("2023-09-23","煌木　太郎の記載、タイトル","煌木　太郎の記載、内容","1",0,CURRENT_TIMESTAMP,CURRENT_TIMESTAMP);
 INSERT INTO daily_report_system.reports(report_date,title,content,employee_code,delete_flg,created_at,updated_at)
      VALUES ("2023-09-23","田中　太郎の記載、タイトル","田中　太郎の記載、内容","2",0,CURRENT_TIMESTAMP,CURRENT_TIMESTAMP);
+INSERT INTO daily_report_system.reports(report_date,title,content,employee_code,delete_flg,created_at,updated_at)
+     VALUES ("2023-09-24","削除フラグtrueの日報タイトル","削除フラグtrueの日報タイトル","1",1,CURRENT_TIMESTAMP,CURRENT_TIMESTAMP);
